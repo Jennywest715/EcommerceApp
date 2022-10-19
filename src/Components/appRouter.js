4829a9de47eea133528270456fd60a8610ccfa7e
unknown <jennywest715@yahoo.com>
2022-10-19 16:37:00 -0700

    adding cart styleing and updating routing

---
+++ src/Components/appRouter.js
@@ -0,0 +1,24 @@
+import { withCart } from "../contexts/cartContext";
+import Products from './products';
+import Cart from './cart';
+import Checkout from "./checkout";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ProductDetails from "./productDetails";
+import Nav from './nav';
+
+function AppRouter(context) {
+    return (
+        <BrowserRouter>
+            <Nav></Nav>
+            <Routes>
+                <Route path="/" element={<Navigate to="/products"/> }/>
+                <Route path="/products" element={ <Products />}/>
+                <Route path="products/:productId" element={ <ProductDetails />}/>
+                <Route path="/cart" element={ <Cart /> }/>
+                <Route path="/checkout" element={ <Checkout /> }/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default withCart(AppRouter)
