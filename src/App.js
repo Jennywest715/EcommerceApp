e5dacd438942525073fa6372cdb79f02b7e848c8
unknown <jennywest715@yahoo.com>
2022-10-19 19:09:35 -0700

    added modules and spelling errors!

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import ProductDetails from './Components/productDetails';
-import Products from './Components/products';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Navigate
-} from "react-router-dom";
+import AppRouter from './Components/appRouter';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/products" />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
-  },
-  {
-    path: "products/:productId",
-    element: <ProductDetails></ProductDetails>,
-  },
-]);
 
 function App() {
+  
   return (
-    <RouterProvider router={router} />
+    <div>
+      <AppRouter />
+    </div>
   );
 }
 
